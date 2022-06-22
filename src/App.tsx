8378d38a3d2852348ae503ba892f9eb829aa8a93
Lucas  FIori <lucas.fiori@corebiz.ag>
2022-06-21 22:27:24 -0300

    Aula 02

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,9 @@
-import { gql, useQuery } from "@apollo/client"
+import { Sidebar } from "./components/Sidebar"
+import { Event } from "./pages/Event"
 
-const GET_LESSONS_QUERY = gql`
-  query {
-    lessons {
-      id
-      title
-    }
-  }`
-
-interface Lesson {
-  id: string;
-  title: string;
-}
 function App() {
-  const { data } = useQuery<{lessons: Lesson[]}>(GET_LESSONS_QUERY)
-
-  console.log(data)
-
   return (
-    <ul>
-      {data?.lessons.map(lessons => (
-        <li key={lessons.id}>{lessons.title}</li>
-      ))}
-    </ul>
+    <Event />
   )
 }
 
